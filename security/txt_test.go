e337f31e9c113d926f405be6e0035d2401a4ae90
Anton Sankov <asankov96@gmail.com>
2023-10-30 23:08:03 +0200

    Add `ParseFromURL` method
    that loads the file from an URL

--- security/txt_test.go
+++ security/txt_test.go
@@ -2,6 +2,9 @@ package security_test
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 	"time"
@@ -13,48 +16,139 @@ import (
 func TestSecurity(t *testing.T) {
 	t.Parallel()
 
-	file, err := os.Open("testdata/security.txt")
-	require.NoError(t, err)
+	testCases := []struct {
+		name   string
+		loadFn func(t *testing.T) (*security.TXT, error)
+	}{
+		{
+			name: "load from file",
+			loadFn: func(t *testing.T) (*security.TXT, error) {
+				file, err := os.Open("testdata/security.txt")
+				require.NoError(t, err)
 
-	t.Cleanup(func() {
-		file.Close()
-	})
+				t.Cleanup(func() {
+					file.Close()
+				})
 
-	txt, err := security.Parse(file)
-	require.NoError(t, err)
+				return security.Parse(file)
+			},
+		},
+		{
+			name: "load from URL - /",
+			loadFn: func(t *testing.T) (*security.TXT, error) {
+				srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					if r.URL.Path != "" && r.URL.Path != "/" {
+						t.Fatalf("Got unexpected path - %s", r.URL.Path)
+						return
+					}
 
-	require.NotNil(t, txt)
+					file, err := os.Open("testdata/security.txt")
+					require.NoError(t, err)
 
-	require.Equal(t, 1, len(txt.Acknowledgments))
-	require.Equal(t, "https://example.com/", txt.Acknowledgments[0])
+					t.Cleanup(func() {
+						file.Close()
+					})
 
-	require.Equal(t, 2, len(txt.Canonical))
-	require.Contains(t, txt.Canonical, "https://www.example.com/.well-known/security.txt")
-	require.Contains(t, txt.Canonical, "https://someserver.example.com/.well-known/security.txt")
+					bytes, err := io.ReadAll(file)
+					require.NoError(t, err)
 
-	require.Equal(t, 2, len(txt.Contact))
-	require.Contains(t, txt.Contact, "https://example.com/vulnz")
-	require.Contains(t, txt.Contact, "mailto:security@example.com")
+					_, err = w.Write(bytes)
+					require.NoError(t, err)
+				}))
+				t.Cleanup(srv.Close)
 
-	require.NotEmpty(t, txt.Encryption)
-	require.Equal(t, "https://example.com/publickey.txt", txt.Encryption)
+				return security.ParseFromURL(srv.URL)
+			},
+		},
+		{
+			name: "load from URL - /security.txt",
+			loadFn: func(t *testing.T) (*security.TXT, error) {
+				srv := serverFor(t, "/security.txt")
 
-	require.False(t, txt.Expires.IsZero())
-	year, month, day := txt.Expires.Date()
-	require.Equal(t, 2021, year)
-	require.Equal(t, time.December, month)
-	require.Equal(t, 31, day)
+				t.Cleanup(srv.Close)
 
-	require.NotEmpty(t, txt.Policy)
-	require.Equal(t, "https://example.com/policy", txt.Policy)
+				return security.ParseFromURL(srv.URL)
+			},
+		},
+		{
+			name: "load from URL - /.well-known/security.txt",
+			loadFn: func(t *testing.T) (*security.TXT, error) {
+				srv := serverFor(t, "/.well-known/security.txt")
 
-	require.NotEmpty(t, txt.Hiring)
-	require.Equal(t, "https://example.com/hiring", txt.Hiring)
+				t.Cleanup(srv.Close)
 
-	require.Equal(t, 3, len(txt.PreferredLanguages))
-	require.Contains(t, txt.PreferredLanguages, "en")
-	require.Contains(t, txt.PreferredLanguages, "es")
-	require.Contains(t, txt.PreferredLanguages, "fr")
+				return security.ParseFromURL(srv.URL)
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			txt, err := testCase.loadFn(t)
+			require.NoError(t, err)
+
+			require.NotNil(t, txt)
+
+			require.Equal(t, 1, len(txt.Acknowledgments))
+			require.Equal(t, "https://example.com/", txt.Acknowledgments[0])
+
+			require.Equal(t, 2, len(txt.Canonical))
+			require.Contains(t, txt.Canonical, "https://www.example.com/.well-known/security.txt")
+			require.Contains(t, txt.Canonical, "https://someserver.example.com/.well-known/security.txt")
+
+			require.Equal(t, 2, len(txt.Contact))
+			require.Contains(t, txt.Contact, "https://example.com/vulnz")
+			require.Contains(t, txt.Contact, "mailto:security@example.com")
+
+			require.NotEmpty(t, txt.Encryption)
+			require.Equal(t, "https://example.com/publickey.txt", txt.Encryption)
+
+			require.False(t, txt.Expires.IsZero())
+			year, month, day := txt.Expires.Date()
+			require.Equal(t, 2021, year)
+			require.Equal(t, time.December, month)
+			require.Equal(t, 31, day)
+
+			require.NotEmpty(t, txt.Policy)
+			require.Equal(t, "https://example.com/policy", txt.Policy)
+
+			require.NotEmpty(t, txt.Hiring)
+			require.Equal(t, "https://example.com/hiring", txt.Hiring)
+
+			require.Equal(t, 3, len(txt.PreferredLanguages))
+			require.Contains(t, txt.PreferredLanguages, "en")
+			require.Contains(t, txt.PreferredLanguages, "es")
+			require.Contains(t, txt.PreferredLanguages, "fr")
+		})
+	}
+}
+
+func serverFor(t *testing.T, path string) *httptest.Server {
+	t.Helper()
+
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != path {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		file, err := os.Open("testdata/security.txt")
+		require.NoError(t, err)
+
+		t.Cleanup(func() {
+			file.Close()
+		})
+
+		bytes, err := io.ReadAll(file)
+		require.NoError(t, err)
+
+		_, err = w.Write(bytes)
+		require.NoError(t, err)
+	}))
 }
 
 func ExampleParse() {
@@ -138,4 +232,14 @@ func TestSecurityErr(t *testing.T) {
 			require.Equal(t, testCase.expectedError, err)
 		})
 	}
+}
+
+func TestParseFromURLNotFound(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	}))
+	t.Cleanup(srv.Close)
+
+	_, err := security.ParseFromURL(srv.URL)
+	require.Error(t, err)
 }
